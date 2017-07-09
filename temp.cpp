d328b95c04eeedaee85edc40cdb07597234574e3
Reason? <Reason@Reasons-MacBook-Air.local>
2017-07-09 20:54:02 +0900

    create template file for cpp

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+#define MOD 1000000007
+
+using namespace std;
+
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  return 0;
+}
