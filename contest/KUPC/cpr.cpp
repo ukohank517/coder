303fa85809e5009a7bde1ada620d14b2719603af
ukohank517 <ukohank517@gmail.com>
2017-10-02 15:29:05 +0900

    [add] KUPC2017

---
+++ contest/KUPC/cpr.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <cmath>
+#include <complex> // complex<double> a(1.2 , 2.3);// real(): 1.2, imag()2.3
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define ld long double
+#define FOR(i,a,b) for(ll i=(ll)a;i<(ll)b;i++)
+#define rep(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rmsame(a) sort(all(a)),a.erase(unique(all(a)), a.end())
+#define rmvector(a,b) rep(i,a.size())rep(j,b.size())if(a[i]==b[j]){a.erase(a.begin()+i);i--;break;}
+template<typename X> bool exist(vector<X> vec, X item){return find(all(vec), item)!=vec.end();}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+
+  ll A;
+  string S;
+  cin >> A >> S;
+
+  ll hash = 0;
+  rep(i,S.size())
+    hash += (pow(A,i+1)*(S[i]-'a'+1));
+  
+  
+  hash /= A;
+  while(hash>0){
+    ll ch = 0;
+    ch = hash % A;
+    hash-=ch;
+    while((ch + A) <= 26 && (hash - A) >=0){
+      ch += A;
+      hash -= A;
+    }
+    hash /= A;
+
+    cout << (char)('a'+ ch -1);
+  }
+
+  cout << endl;
+  return 0;
+}
