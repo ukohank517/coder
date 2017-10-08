6c8d9702e0a8bdf6280c7bd7954ace5839333b36
Reason? <ukohank517@gmail.com>
2017-10-08 20:53:37 +0900

    289,Dは途中まで

---
+++ contest/ddcc2017/c.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <cmath>
+#include <complex> // complex<double> a(1.2 , 2.3);// real(): 1.2, imag()2.3
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define ld long double
+#define FOR(i,a,b) for(ll i=(ll)a;i<(ll)b;i++)
+#define rep(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rmsame(a) sort(all(a)),a.erase(unique(all(a)), a.end())
+#define rmvector(a,b) rep(i,a.size())rep(j,b.size())if(a[i]==b[j]){a.erase(a.begin()+i);i--;break;}
+template<typename X> bool exist(vector<X> vec, X item){return find(all(vec), item)!=vec.end();}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  ll N ,C;
+  cin >> N >> C;
+  vector<ll> L;
+  rep(i,N){
+    ll inp;cin>> inp;
+    L.pb(inp);
+  }
+
+  sort(all(L));
+  
+  ll ans = 0;
+
+  ll memo = 0;
+  for(ll i =L.size()-1;i>=0;i--){
+    if(memo==i) {ans ++; break;}
+    if(memo>i)break;
+    if((L[i]+L[memo]+1) <= C){
+      memo++;
+      ans++;
+    }
+    else ans++;
+  }
+  cout << ans << endl;
+
+  //cout << fixed << setprecision(16) << ans << endl;
+  return 0;
+}
