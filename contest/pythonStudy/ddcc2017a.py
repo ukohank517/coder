59e72b19ac48e8189691ccf5bf877ede67c6ea91
ukohank517 <ukohank517@gmail.com>
2017-10-14 10:03:58 +0900

    [add] learned: input, list input

---
+++ contest/pythonStudy/ddcc2017a.py
@@ -0,0 +1,6 @@
+#DDCC -> Yes , else No
+
+#reference:
+#https://beta.atcoder.jp/contests/ddcc2017-qual/submissions/1661752
+(a,b,c,d) = input()
+print(['No','Yes'][a==b!=c==d])
---
+++ contest/pythonStudy/ddcc2017b.py
@@ -0,0 +1,3 @@
+#a,b,c,d = map(int ,input().split()
+a,b,c,d = map(int,input().split())
+print(a*1728+b*144+c*12+d)
