5499afb0a5541efa9fcd8ac7c50a2630ef153b15
ukohank517 <ukohank517@gmail.com>
2019-06-26 08:46:42 +0900

    [add] prime 素数列挙、判別、数え上げ

---
+++ library/number_structure/PrimeNumbers.cpp
@@ -0,0 +1,38 @@
+struct PrimeNumbers {
+    
+    vector<bool> sieve;
+    vector<long long> prime;
+    
+    PrimeNumbers (int n) : sieve(n+1,true) {
+        sieve[0] = sieve[1] = false;
+        for (long long i = 2; i < sieve.size(); i++) {
+            if (sieve[i]) {
+                prime.push_back(i);
+                for (long long j = i * i; j < sieve.size(); j += i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+    }
+    
+    bool isPrime(long long n) {
+        return sieve[n];
+    }
+    
+    int size(void) {
+        return (int) prime.size();
+    }
+    
+    long long  operator [] (int n) {
+        return  prime[n];
+    }
+    
+};
+/*
+ O(N loglogN) ←早い!
+ usage:
+ PrimeNumbers prime(N); // Nまでの素数を列挙
+ prime.size();  //素数の数
+ prime[i]; //i番目の素数
+ prime.isPrime(A); // bool, Aの素数判別
+ */
