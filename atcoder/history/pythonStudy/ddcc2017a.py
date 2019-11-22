00390cfe3008394f7474a010d2f9f0a2ede93726
ukohank517 <ukohank517@gmail.com>
2019-11-22 20:14:24 +0900

    [clear] history

--- atcoder/history/pythonStudy/ddcc2017a.py
+++
@@ -1,6 +0,0 @@
-#DDCC -> Yes , else No
-
-#reference:
-#https://beta.atcoder.jp/contests/ddcc2017-qual/submissions/1661752
-(a,b,c,d) = input()
-print(['No','Yes'][a==b!=c==d])
--- atcoder/history/pythonStudy/ddcc2017b.py
+++
@@ -1,3 +0,0 @@
-#a,b,c,d = map(int ,input().split()
-a,b,c,d = map(int,input().split())
-print(a*1728+b*144+c*12+d)
